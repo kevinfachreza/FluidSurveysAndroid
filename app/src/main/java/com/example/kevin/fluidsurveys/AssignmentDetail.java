f2c08cf14a3d60b3942c95f939beac014de657bf
kevinfachreza <kevinfachreza@yahoo.com>
2017-05-18 18:43:51 +0700

    activity assignment, assignment detail

---
+++ app/src/main/java/com/example/kevin/fluidsurveys/AssignmentDetail.java
@@ -0,0 +1,13 @@
+package com.example.kevin.fluidsurveys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AssignmentDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment_detail);
+    }
+}
